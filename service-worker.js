008600a0449f24a3ac56da271c8d43eeef238b73
richardtim230 <134277464+richardtim230@users.noreply.github.com>
2024-11-29 01:10:32 +0100

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'student-support-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/app.js',
+  'https://cdn.quilljs.com/1.3.7/quill.min.js',
+  'https://cdn.quilljs.com/1.3.7/quill.snow.css',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
